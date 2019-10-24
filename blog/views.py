7a264357f7371fdfbe23da009df4f47745a9e55c
yupple <popo.noelu@gmail.com>
2019-10-24 12:56:17 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -15,8 +14,6 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-@login_required
-
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,8 +26,6 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
-@login_required
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -46,20 +41,14 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-@login_required
-
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
-
-@login_required
 
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
-
-@login_required
 
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
