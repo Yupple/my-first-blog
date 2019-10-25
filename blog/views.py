42b37033e264950d370f384ea39acc211e40ab26
yupple <popo.noelu@gmail.com>
2019-10-25 23:52:03 +0900

    all5

--- blog/views.py
+++ blog/views.py
@@ -13,10 +13,10 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-@login_required
+
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = CommentForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
